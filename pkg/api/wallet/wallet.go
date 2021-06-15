3bae14356324f1a0a7b3cf330a85bd39854134b4
jeff2857 <jeffliu2857@gmail.com>
2021-06-15 22:06:44 +0800

    feat: add fast withdraw api

--- pkg/api/wallet/wallet.go
+++ pkg/api/wallet/wallet.go
@@ -11,8 +11,11 @@ var bClient = &client.Client{}
 
 // endpoint
 const (
-	EP_SYSTEM_STATUS = "/sapi/v1/system/status"
-	EP_ALL_CAPITAL   = "/sapi/v1/capital/config/getall"
+	EP_SYSTEM_STATUS         = "/sapi/v1/system/status"
+	EP_ALL_CAPITAL           = "/sapi/v1/capital/config/getall"
+	EP_ACCOUNT_SNAPSHOT      = "/sapi/v1/accountSnapshot"
+	EP_ENABLE_FAST_WITHDRAW  = "/sapi/v1/account/enableFastWithdrawSwitch"
+	EP_DISABLE_FAST_WITHDRAW = "/sapi/v1/account/disableFastWithdrawSwitch"
 )
 
 // get system status
@@ -29,8 +32,44 @@ func GetSystemStatus() ([]byte, error) {
 // get all capital
 func GetAllCapital() ([]byte, error) {
 	params := make(map[string]interface{})
-	params["recvWindow"] = 50000
+	params["recvWindow"] = 5000
 	res, err := bClient.Request(methods.GET, EP_ALL_CAPITAL, params, true, true)
+	if err != nil {
+		log.Fatal(err)
+		return nil, err
+	}
+
+	return res, nil
+}
+
+// get account snapshot
+// t type "SPOT" | "MARGIN" | "FUTURES"
+func GetAccountSnapshot(t string) ([]byte, error) {
+	params := make(map[string]interface{})
+	params["type"] = t
+	res, err := bClient.Request(methods.GET, EP_ACCOUNT_SNAPSHOT, params, true, true)
+	if err != nil {
+		log.Fatal(err)
+		return nil, err
+	}
+
+	return res, nil
+}
+
+// enable fast withdraw
+func EnableFastWithdraw() ([]byte, error) {
+	res, err := bClient.Request(methods.POST, EP_ENABLE_FAST_WITHDRAW, nil, true, true)
+	if err != nil {
+		log.Fatal(err)
+		return nil, err
+	}
+
+	return res, nil
+}
+
+// disable fast withdraw
+func DisableFastWithdraw() ([]byte, error) {
+	res, err := bClient.Request(methods.POST, EP_DISABLE_FAST_WITHDRAW, nil, true, true)
 	if err != nil {
 		log.Fatal(err)
 		return nil, err
